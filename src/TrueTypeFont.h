8c20078536eda1ed0187443b53ae0b7a21da56fa
Jeremie Roy <jeremie.roy@gmail.com>
2013-04-18 12:53:48 +0200

    cleaning and comments improvements

--- src/TrueTypeFont.h
+++ src/TrueTypeFont.h
@@ -16,26 +16,24 @@ public:
 	/// @remark The ownership of the buffer is external, and you must ensure it stays valid up to this object lifetime
 	/// @return true if the initialization succeed
     bool init(const uint8_t* buffer, uint32_t bufferSize, int32_t fontIndex, uint32_t pixelHeight );
-
-	/// seems hacky :s
-	bool isValid() { return m_font!=NULL; }
-
+	
 	/// return the font descriptor of the current font
 	FontInfo getFontInfo();
-
-	/// return the details of a glyph
-	/// return false if the glyph couldn't be found
-	//bool getGlyphInfo(const FontInfo& fontInfo, CodePoint_t codePoint, GlyphInfo& outGlyphInfo);
-
+	
 	/// raster a glyph as 8bit alpha to a memory buffer
+	/// update the GlyphInfo according to the raster strategy
 	/// @ remark buffer min size: glyphInfo.width * glyphInfo * height * sizeof(char)
-    bool bakeGlyphAlpha(const FontInfo& fontInfo,CodePoint_t codePoint, GlyphInfo& outGlyphInfo, uint8_t* outBuffer);
+    bool bakeGlyphAlpha(const FontInfo& fontInfo, CodePoint_t codePoint, GlyphInfo& outGlyphInfo, uint8_t* outBuffer);
 
-	/// raster a glyph as 32bit rgba to a memory buffer
+	/// raster a glyph as 32bit subpixel rgba to a memory buffer
+	/// update the GlyphInfo according to the raster strategy
 	/// @ remark buffer min size: glyphInfo.width * glyphInfo * height * sizeof(uint32_t)
-    bool bakeGlyphSubpixel(const FontInfo& fontInfo,CodePoint_t codePoint, GlyphInfo& outGlyphInfo, uint8_t* outBuffer);
+    bool bakeGlyphSubpixel(const FontInfo& fontInfo, CodePoint_t codePoint, GlyphInfo& outGlyphInfo, uint8_t* outBuffer);
 
-	bool bakeGlyphDistance(const FontInfo& fontInfo,CodePoint_t codePoint, GlyphInfo& outGlyphInfo, uint8_t* outBuffer);
+	/// raster a glyph as 8bit signed distance to a memory buffer
+	/// update the GlyphInfo according to the raster strategy
+	/// @ remark buffer min size: glyphInfo.width * glyphInfo * height * sizeof(char)
+	bool bakeGlyphDistance(const FontInfo& fontInfo, CodePoint_t codePoint, GlyphInfo& outGlyphInfo, uint8_t* outBuffer);
 private:
 	void* m_font;
 };
